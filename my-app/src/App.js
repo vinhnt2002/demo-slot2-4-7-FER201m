3470841dfc20896c91ba841c7f0b4f1d67f996e7
vinh nguyễn <vinhntse2002@gmail.com>
2023-05-17 23:50:40 +0700

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div >
+        <nav>
+                <ul >
+                    <li ><a className='active' href='#home'>Home</a></li>
+                    <li ><a href='#news'>News</a></li>
+                    <li ><a href='#about'>About</a></li>
+                    <li ><a href='#contact'>Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+  );
+}
+
+export default App;
